81f06e925168db36dd8724344c949f23cd7ec0af
aniruddha <aniruddha.kudalkar@gmail.com>
2020-09-12 13:13:16 +0530

    dom - manipulation of dom

--- day1/app.js
+++ day1/app.js
@@ -5,5 +5,14 @@ console.log(dv)
 
 let ce = document.getElementById('ce')
 console.log(ce)
-
 console.log(console)
+
+let mobiles = ['Android', 'iOS', 'BB', 'Windows', 'PalmTop', 'Samsung']
+
+let olMob = document.getElementById('mob')
+
+for (let i = 0; i < mobiles.length; i++) {
+    let li = document.createElement('li')
+    li.textContent = mobiles[i]
+    olMob.appendChild(li)
+}
