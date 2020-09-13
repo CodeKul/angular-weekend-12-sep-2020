e3bdee42aeaa630630c703d6dcd8826fbc92d267
aniruddha <aniruddha.kudalkar@gmail.com>
2020-09-13 12:54:58 +0530

    typescript : types

--- day2/types.ts
+++ day2/types.ts
@@ -3,8 +3,21 @@ var age: number = 10
 var isMob: boolean = true
 var sal = 15.6
 
-function myName(isMob: boolean): void {
+var obj: {} = { dp: 'njk', id: 10 }
+var objSp: { [key: string]: boolean } = { age: true }
+
+var mixed: number | boolean | string = true
+
+var fn: (isOk: boolean) => void = function (isOk: boolean): void {
+}
+
+fn = myName
+
+var anything: any = {}
+
+function myName(isMob: boolean): boolean {
     nm = 'sdssf'
+    return 'hi' == 'hi'
 }
 
 myName(true)
